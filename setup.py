d33fd217451bf29d8d055e2e03428224b369d496
William Schoenell <wschoenell@gmail.com>
2015-07-03 15:31:21 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='chimera-serial',
+    name='chimera-lna',
     version='0.0.1',
-    packages=['chimera_serial', 'chimera_serial.instruments'],
+    packages=['chimera_lna', 'chimera_lna.instruments'],
     scripts=[],
-    url='http://github.com/astroufsc/chimera-serial',
+    url='http://github.com/astroufsc/chimera-lna',
     license='GPL v2',
     author='William Schoenell',
     author_email='william@iaa.es',
     install_requires=['pyserial'],
-    description='Chimera plugin for serial devices'
+    description='Chimera plugin for LNA domes'
 )
